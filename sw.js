564b1904def0bc8de095fe3f35b438945ff6b1e4
test1800 <mimabdibcc@gmail.com>
2026-08-08 17:20:31 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -409,3 +409,56 @@ async function cacheFirst(
 
 
 }
+
+/* =====================================================
+   NOTIFICATION CLICK
+===================================================== */
+
+self.addEventListener(
+    "notificationclick",
+    event => {
+
+        event.notification.close();
+
+        event.waitUntil(
+
+            clients.matchAll({
+                type: "window",
+                includeUncontrolled: true
+            })
+
+            .then(
+                clientList => {
+
+                    for (
+                        const client
+                        of clientList
+                    ) {
+
+                        if (
+                            "focus" in client
+                        ) {
+
+                            return client.focus();
+
+                        }
+
+                    }
+
+                    if (
+                        clients.openWindow
+                    ) {
+
+                        return clients.openWindow(
+                            "./"
+                        );
+
+                    }
+
+                }
+            )
+
+        );
+
+    }
+);
